2312e925e8cebd818a091d99d55fe6b4b0cc06ce
Lucas Lima <lucalmendes9@gmail.com>
2022-05-09 20:14:08 -0300

    fix: lint

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,6 +1,6 @@
 import './styles.css'
 import Icon from '@mdi/react'
-import { mdiAccount, mdiBriefcase, mdiBriefcaseOutline, mdiChartBar, mdiComment, mdiCommentOutline, mdiDotsVertical, mdiExitToApp, mdiLightningBolt, mdiLightningBoltOutline, mdiSprout, mdiViewDashboard } from '@mdi/js'
+import { mdiBriefcase, mdiChartBar, mdiComment, mdiCommentOutline, mdiDotsVertical, mdiExitToApp, mdiLightningBolt, mdiSprout, mdiViewDashboard } from '@mdi/js'
 
 const SideBar = () => {
     return (
@@ -12,9 +12,9 @@ const SideBar = () => {
                         <div class="dropdown">
                             <button class="dropbtn"><Icon path={mdiDotsVertical} /></button>
                             <div class="dropdown-content">
-                                <a href="#">Link 1</a>
-                                <a href="#">Link 2</a>
-                                <a href="#">Link 3</a>
+                                <a href="javascript:void(0)">Link 1</a>
+                                <a href="javascript:void(0)">Link 2</a>
+                                <a href="javascript:void(0)">Link 3</a>
                             </div>
                         </div>
                     </div>
@@ -29,35 +29,35 @@ const SideBar = () => {
 
                 <div className="content-buttons">
                     <div className="Home">
-                        <a href="#"> <Icon path={mdiViewDashboard} /> Home</a>
+                        <a href="javascript:void(0)"> <Icon path={mdiViewDashboard} /> Home</a>
                     </div>
 
                     <div className="Plantacao">
-                        <a href="#"> <Icon path={mdiSprout} /> Plantação</a>
+                        <a href="javascript:void(0)"> <Icon path={mdiSprout} /> Plantação</a>
                     </div>
 
                     <div className="Estoque">
-                        <a href="#"> <Icon path={mdiChartBar} /> Estoque</a>
+                        <a href="javascript:void(0)"> <Icon path={mdiChartBar} /> Estoque</a>
                     </div>
 
                     <div className="Sensor">
-                        <a href="#"><Icon path={mdiLightningBolt} /> Sensores</a>
+                        <a href="javascript:void(0)"><Icon path={mdiLightningBolt} /> Sensores</a>
                     </div>
 
                     <div className="Consultoria">
-                        <a href="#"> <Icon path={mdiBriefcase} /> Consultoria</a>
+                        <a href="javascript:void(0)"> <Icon path={mdiBriefcase} /> Consultoria</a>
                     </div>
                 </div>
 
                 <div className="content-Mensagens">
                     <div className="Mensagens">
-                        <a href="#"> <Icon path={mdiComment} /> Mensagens</a>
+                        <a href="javascript:void(0)"> <Icon path={mdiComment} /> Mensagens</a>
                     </div>
                 </div>
 
                 <div className="content-Sair">
                     <div className="Sair">
-                        <a href="#"> <Icon path={mdiExitToApp} /> Sair</a>
+                        <a href="javascript:void(0)"> <Icon path={mdiExitToApp} /> Sair</a>
                     </div>
                 </div>
             </div>
--- src/pages/Sensor/index.js
+++ src/pages/Sensor/index.js
@@ -1,5 +1,5 @@
 import './styles.css'
-import React, { Component } from 'react'
+import React from 'react'
 import SideBar from '../../components/SideBar'
 
 const Sensor = () => {
