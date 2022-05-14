68940c3baf58e3667fe4169bd559ed6e957f2ff0
Lucas Lima <lucalmendes9@gmail.com>
2022-05-14 14:21:05 -0300

    feat: new tests to pages

--- src/tests/integration/Home.spec.js
+++ src/tests/integration/Home.spec.js
@@ -1,8 +1,13 @@
 describe('Test home page', () => {
-
     it('home page see sidebar', () => {
         cy.visit('');
         cy.get('.SideBar').should('be.visible');
     });
 
+    it('home page see cards cta', () => {
+        cy.visit('');
+        cy.contains('Noticia');
+        cy.contains('Clima');
+        cy.contains('Quantidade');
+    });
 });
--- src/tests/integration/Sensor.spec.js
+++ src/tests/integration/Sensor.spec.js
@@ -1,8 +1,13 @@
 describe('Test sensor page', () => {
-
     it('sensor page see sidebar', () => {
         cy.visit('/sensor');
         cy.get('.SideBar').should('be.visible');
     });
 
+    it('home page see cards cta', () => {
+        cy.visit('/sensor');
+        cy.contains('Temperatura');
+        cy.contains('Luminosidade');
+        cy.contains('Umidade');
+    });
 });
